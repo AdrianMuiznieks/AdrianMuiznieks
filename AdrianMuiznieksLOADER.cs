f290868a89156ad1ca559b20f377f9142c83a6a2
Adrian <149612777+AdrianMuiznieks@users.noreply.github.com>
2023-12-09 11:27:36 +0000

    AdrianMuiznieksLOADER.cs
    Update 1

---
+++ AdrianMuiznieksLOADER.cs
@@ -0,0 +1,6 @@
+<!DOCTYPE html 6 python 7:<!6></!6>> 
+<cyp content=<text> (json; (Make) addText=".cyp">
+</text>
+<doctype meta="charset: UTF-8" append>
+<doctype svg=none width=80 height=80 apply=n layer=4L f"{doctype}"=apply.append.play({r: "r"}, [g, l, x, y, z])={doc}.cyp()
+    dd="d.com3html"
