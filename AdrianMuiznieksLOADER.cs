277a09a7bd12d38fc007697ce07c4f7818750fb5
Adrian <149612777+AdrianMuiznieks@users.noreply.github.com>
2023-12-20 17:44:15 +0000

    Update AdrianMuiznieksLOADER.cs

--- AdrianMuiznieksLOADER.cs
+++ AdrianMuiznieksLOADER.cs
@@ -1,6 +1,22 @@
 <!DOCTYPE html 6 python 7:<!6></!6>> 
-<cyp content=<text> (json; (Make) addText=".cyp">
-</text>
+<cyp content=<text> 
+     (json; (Make) 
+     addText=".cyp"
+>
+
 <doctype meta="charset: UTF-8" append>
-<doctype svg=none width=80 height=80 apply=n layer=4L f"{doctype}"=apply.append.play({r: "r"}, [g, l, x, y, z])={doc}.cyp()
+<doctype svg=none 
+         width=80 
+         height=80 
+         apply=converthash{
+             const hash = output.("glxyx-cd")
+             const firstCYP = require("cyp")
+             const lastCYP = firstCYP.get(last)
+
+             function getCyp(firstCYP, lastCYP)
+         }
+         layer=4L 
+         f"{doctype}"=apply.append.play(
+            {r: "r"}, 
+            [g, l, x, y, z])={doc}.cyp()
     dd="d.com3html"
